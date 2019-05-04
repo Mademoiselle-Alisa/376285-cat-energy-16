d05f267d9f126440d563542994f240af8f825f48
Kseniia Karasenko <mademoiselle.alisa@gmail.com>
2019-05-04 17:31:26 +0300

    Доработки

---
+++ source/js/main-nav-btn.js
@@ -0,0 +1,11 @@
+var mainNavBtn = document.querySelector(".main-navigation__toggle");
+var mainNavMenu = document.querySelector(".main-navigation__list"); 
+  
+
+  mainNavBtn.addEventListener("click", 
+    function (evt) {
+    evt.preventDefault();
+	mainNavBtn.classList.toggle("main-navigation__toggle--open");
+    mainNavMenu.classList.toggle("list-show");
+  }
+);
