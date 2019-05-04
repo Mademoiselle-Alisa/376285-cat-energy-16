03bb30048bf4f862171a43487779bf543a6be7f4
Kseniia Karasenko <mademoiselle.alisa@gmail.com>
2019-05-04 17:44:00 +0300

    StyleLint

--- source/js/main-nav-btn.js
+++ source/js/main-nav-btn.js
@@ -1,11 +1,9 @@
 var mainNavBtn = document.querySelector(".main-navigation__toggle");
-var mainNavMenu = document.querySelector(".main-navigation__list"); 
-  
-
-  mainNavBtn.addEventListener("click", 
-    function (evt) {
-    evt.preventDefault();
-	mainNavBtn.classList.toggle("main-navigation__toggle--open");
-    mainNavMenu.classList.toggle("list-show");
+var mainNavMenu = document.querySelector(".main-navigation__list");
+mainNavBtn.addEventListener("click",
+  function (evt) {
+  evt.preventDefault();
+  mainNavBtn.classList.toggle("main-navigation__toggle--open");
+  mainNavMenu.classList.toggle("list-show");
   }
 );
